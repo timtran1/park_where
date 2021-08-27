b2c8e937fd564a06dea8134b373af1879c935120
trung@kiuglobal.com <trung@kiuglobal.com>
2021-08-27 21:32:17 +0700

    add files

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM arm32v6/node:alpine
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
+
+RUN npm rebuild --build-from-source grpc
+RUN npm install
+# If you are building your code for production
+# RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
+
+EXPOSE 3000
+CMD [ "npm", "start" ]
---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const express = require('express')
+var cors = require('cors')
+const app = express()
+app.use(cors())
+const port = 3000
+
+
+app.get('/timpark/api/get', (req, res) => {
+    fs.readFile('home/tim/park_where/db/db.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            res.send('error')
+        }
+        res.send(data)
+    })
+})
+
+app.get('/timpark/api/set', (req, res) => {
+    let content = req.query.floor
+
+    fs.writeFile('/home/tim/park_where/db/db.txt', content, err => {
+        if (err) {
+            console.error(err)
+            res.send('error')
+        }
+        res.send('success')
+    })
+})
+
+app.listen(port, () => {
+    console.log(`ParkWhere app listening at http://localhost:${port}`)
+})
